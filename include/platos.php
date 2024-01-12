ff73f3d593e23b660528667eac9f555a0a575054
Marco Antonio Andrade Guerrero <tecmarcoantonio@gmail.com>
2024-01-12 04:17:22 -0600

    mejoras con bonos extras

--- include/platos.php
+++ include/platos.php
@@ -1,34 +1,84 @@
-  <?php
-    $platos = "select * from platos";
-    $resultadoPlatos = mysqli_query($cnn,$platos);
-  ?>
+      <?php
+        $plato="select * from platos";
+        $resultplato = mysqli_query($cnn,$plato);
+      ?>
+        <div class="container contenido">
+      <div class="row">
+      	<!-- Platos del día-->
 
-  <div class="container contenido">
-  <div class="row">
-    <?php
-    while($row3 = mysqli_fetch_array($resultadoPlatos)){
-               echo'<div class="col-md-4">';
-                  echo'<div class="card mb-4 shawdow-sm">';
-                     echo'<img src="img/'.$row3["imagen"].'">';
-                     echo'<div class="card-body">';
-                        echo' <p class="card-text">';
-                            echo $row3["detalle"];  
-                         echo'</p>';
-                         echo'<div class="d-flex justify-content-between align-items-center">';
-                             echo'<div class="btn-group">';
-                                   echo'<a href='.$row3["urlDetalle"].'><button class="btn btn-sm btn-outline-secondary" type="button">';
-                                        echo' Detalles </a>';
-                                   echo'</button>';
-                                   echo'<a href='.$row3["urlReservar"].'><button class="btn btn-sm btn-outline-secondary" type="button">';
-                                         echo'Reservar</a>';
-                                   echo'</button>';
-                             echo'</div>';
-                             echo'<small class="text-muted">'.$row3["tiempoPreparacion"].' minutos</small>';  
-                        echo' </div> '; 
-                     echo'</div> ';  
-                  echo'</div>';   
-               echo'</div>';
-               }
- ?>
- </div>
-        
+         <?php
+              while($fila3 = mysqli_fetch_array($resultplato)){
+
+                  echo '<div class="col-md-4">';
+                    echo '<div class="card mb-4 shadow-sm">';
+                      echo '<img src="img/'.$fila3["imagen"].'">';
+                      echo '<div class="card-body">';
+                        echo '<p class="card-text">'.$fila3["detalle"].'</p>';
+                        echo '<div class="d-flex justify-content-between align-items-center">';
+                          echo '<div class="btn-group">';
+                           echo ' <a data-target="'.$fila3["urlDetalle"].$fila3['id'].'" class="btn btn-sm btn-outline-secondary" data-toggle="modal">Detalles</a>';
+                            echo '<a href="'.$fila3["urlReservar"].'" class="btn btn-sm btn-outline-secondary">Reservar</a>';
+                          echo '</div>';
+                          echo '<small class="text-muted">'.$fila3["tiempoPreparacion"].' mins</small>';
+                        echo '</div>';
+                      echo '</div>';
+                    echo '</div>';
+                  echo '</div>';
+
+          ?>
+
+          <!-- Modal -->
+            <div class="modal fade" id="detalle<?php echo $fila3["id"];?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                      <?php echo $fila3['NombrePlato']; ?>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="row">
+                      <div class="col-6">
+                        <img src="img/<?php echo $fila3['imagen'];?>" style="width: 100%;">
+                      </div>
+                       <div class="col-6">
+                        <p>
+                          <?php echo $fila3['detalle']; ?>
+                        </p>
+                        <p>
+                          Tiempo de preparación: <?php echo $fila3['tiempoPreparacion']; ?> minutos
+                        </p>
+                  
+                        <button type="button" class="btn btn-danger">
+                            Precio $ <span class="badge badge-light"><?php echo $fila3['Precio']; ?></span>
+                          </button>
+                      </div>
+                      
+                      
+                    </div>
+                  </div>
+    
+                </div>
+              </div>
+            </div>
+
+          <?php
+
+                }
+          ?>
+
+    
+      
+  
+
+      
+
+
+      
+    
+        <!-- Platos del día-->
+
+      </div>
